d8c205aa0d0a023ca994d03285d34f1f0822274f
Maisam Afshar <maisam.afshar@gmail.com>
2023-01-09 23:42:41 +0430

    config: color and backgroundImage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,24 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "purple-light": "#514CFF",
+        "purple-dark": "#2A27C9",
+        "dark-100": "#060714",
+        "dark-90": "#262840",
+        "gray-100": "#7780A1",
+        "gray-50": "#BBC0D0",
+        "gray-20": "#E2E2ED",
+        "gray-10": "#F2F3F6",
+        green: "#76DECC",
+        pink: "#EA6CBC",
+        red: "#FB4539",
+      },
       fontFamily: {
         sans: ["Sora", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        background: "url('./assets/background.svg')",
       },
     },
   },
