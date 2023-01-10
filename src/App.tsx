b1ebaf7c7132d823e8eb7b3f099dd45d6e1fea77
Maisam Afshar <maisam.afshar@gmail.com>
2023-01-10 14:40:31 +0430

    add header, auctions and overline sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import { useState } from "react";
-import "./App.css";
+import Auctions from "./sections/Auctions";
+import Header from "./sections/Header";
 
+// images
+import iconBucket from "./assets/bucket.svg";
+import Sales from "./sections/Sales";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+    <div className="relative h-screen w-full">
+      <Header />
+      <Auctions />
+      <section className="w-8/12 mx-auto pb-16">
+        <div className="flex md:flex-row flex-col justify-between items-center">
+          <div className="flex-1 font-thin flex flex-col justify-end">
+            <p className="uppercase text-sm text-gray-100">Overline</p>
+            <h2 className="text-6xl mt-2 mb-4 font-bold leading-tight">
+              Sapien ipsum <br /> scelerisque <br /> convallis fusce
+            </h2>
+            <p className="text-gray-100 text-sm max-w-md">
+              Ut amet vulputate faucibus vitae semper eget auctor. Diam tempor
+              pulvinar ultricies dolor feugiat aliquam commodo.
+            </p>
+            <div className="flex gap-3 mt-8">
+              <button
+                type="button"
+                className="bg-gradient-to-r rounded-xl from-[#4745D0] to-purple-dark text-sm text-white px-4 py-2"
+              >
+                Get started
+              </button>
+              <button
+                type="button"
+                className="rounded-xl border border-gray-100 text-sm text-gray-100 px-4 py-2"
+              >
+                Learn more
+              </button>
+            </div>
+          </div>
+          <div className="flex-1 flex justify-end">
+            <img src={iconBucket} alt="" className="" />
+          </div>
+        </div>
+      </section>
+      <Sales />
     </div>
   );
 }
