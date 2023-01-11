b329c0d4a9a520b4b740c6e5c4c943bb5a0149ae
Maisam Afshar <maisam.afshar@gmail.com>
2023-01-11 11:39:08 +0430

    conditionally render avatars and price

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,13 +3,13 @@ import iconFavorite from "../assets/icon-favorite.svg";
 
 type Props = {
   thumbnail: string;
-  avatars: string[];
+  avatars?: string[];
   title: string;
   price: string;
   timeLeft: string;
   peopleBidding: string;
   likes: number;
-  className: string;
+  className: { width: string; height: string };
 };
 
 export default function Card({
@@ -23,35 +23,52 @@ export default function Card({
   className,
 }: Props) {
   return (
-    <div className={`flex flex-col w-[22rem] shrink-0 ${className}`}>
+    <div className={`flex flex-col shrink-0 ${className.width}`}>
       <img
         src={thumbnail}
-        alt=""
-        className="h-[30rem] w-full drop-shadow-lg rounded-xl object-cover"
+        alt={title}
+        className={`w-full ${className.height} drop-shadow-lg rounded-xl object-cover`}
       />
       <div className="flex justify-between space-x-2 mt-3">
-        <h4 className="text-wrap break-words text-start">{title}</h4>
-        <p className="bg-purple-dark/10 text-purple-dark min-w-max text-xs h-fit px-3 py-2">
-          {price}
-        </p>
+        <h4 className="text-wrap break-words text-start line-clamp-2">
+          {title}
+        </h4>
+        {avatars && (
+          <p className="bg-purple-dark/10 text-purple-dark min-w-max text-xs h-fit px-3 py-2">
+            {price}
+          </p>
+        )}
       </div>
-      <div className="flex gap-3 mt-6 mb-4">
-        <img src={iconClock} alt="" className="inline" />
-        <p className="inline text-sm text-gray-50">{timeLeft}</p>
-      </div>
+      {avatars ? (
+        <div className="flex gap-3 mt-6 mb-4">
+          <img src={iconClock} alt="" className="inline" />
+          <p className="inline text-sm text-gray-50">{timeLeft}</p>
+        </div>
+      ) : (
+        <div className="flex justify-between mt-6 mb-4">
+          <div className="flex gap-3">
+            <img src={iconClock} alt="" className="inline" />
+            <p className="inline text-xs text-gray-100 font-thin">{timeLeft}</p>
+          </div>
+          <p className="bg-purple-dark/10 text-purple-dark min-w-max text-xs h-fit px-2 py-1">
+            {price}
+          </p>
+        </div>
+      )}
       <hr />
       <div className="flex justify-between mt-2">
         <div className="flex gap-2 items-center">
           <div className="flex">
-            {avatars?.map((avatar) => {
-              return (
-                <img
-                  src={avatar}
-                  alt=""
-                  className="border rounded-full border-white -ml-4 h-10 w-10"
-                />
-              );
-            })}
+            {avatars &&
+              avatars.map((avatar) => {
+                return (
+                  <img
+                    src={avatar}
+                    alt=""
+                    className="border rounded-full border-white -ml-4 h-10 w-10"
+                  />
+                );
+              })}
           </div>
           <p className="text-xs font-thin text-gray-100 min-w-max">
             {peopleBidding}
